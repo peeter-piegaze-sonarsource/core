eb27f2bcb2992b2401d88438687d76db2f8a6572
smichea <sebastien.michea@manaty.net>
2015-03-23 06:47:22 +0100

    fixed lazy loading issue

--- meveo-admin/ejbs/src/main/java/org/meveo/cache/RatingCacheContainerProvider.java
+++ meveo-admin/ejbs/src/main/java/org/meveo/cache/RatingCacheContainerProvider.java
@@ -27,6 +27,7 @@ import org.meveo.model.cache.CounterPeriodCache;
 import org.meveo.model.cache.TriggeredEDRCache;
 import org.meveo.model.cache.UsageChargeInstanceCache;
 import org.meveo.model.cache.UsageChargeTemplateCache;
+import org.meveo.model.catalog.Calendar;
 import org.meveo.model.catalog.PricePlanMatrix;
 import org.meveo.model.catalog.TriggeredEDRTemplate;
 import org.meveo.model.catalog.UsageChargeTemplate;
@@ -140,7 +141,7 @@ public class RatingCacheContainerProvider {
                 pricePlan.getOfferTemplate().getCode();
             }
             if (pricePlan.getValidityCalendar() != null) {
-                pricePlan.getValidityCalendar().getCode();
+            	preloadCalendar(pricePlan.getValidityCalendar());
             }
 
             log.info("Added pricePlan to cache for provider=" + pricePlan.getProvider().getCode() + "; chargeCode=" + pricePlan.getEventCode() + "; priceplan=" + pricePlan);
@@ -155,6 +156,12 @@ public class RatingCacheContainerProvider {
         log.debug("Price plan cache populated with {} price plans", activePricePlans.size());
     }
 
+    
+    public void preloadCalendar(Calendar calendar){
+    	log.debug("preloadCalendar {} : {}",calendar.getCode(),calendar.getClass());
+    	calendar.nextCalendarDate(new Date());
+    }
+    
     /**
      * Add price plan to a cache
      * 
@@ -186,7 +193,7 @@ public class RatingCacheContainerProvider {
             pricePlan.getOfferTemplate().getCode();
         }
         if (pricePlan.getValidityCalendar() != null) {
-            pricePlan.getValidityCalendar().getCode();
+        	preloadCalendar(pricePlan.getValidityCalendar());
         }
 
         chargePriceList.add(pricePlan);
