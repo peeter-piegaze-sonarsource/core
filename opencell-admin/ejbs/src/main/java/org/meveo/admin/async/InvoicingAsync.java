opencellsoft/opencell-core

Review #5687 recalculate collection date invoice/AO

opencell-admin/ejbs/src/main/java/org/meveo/admin/async/InvoicingAsync.java
@@ -213,7 +213,8 @@ public class InvoicingAsync {
                 break;
             }
             try {
-                invoiceService.assignInvoiceNumberAndIncrementBAInvoiceDate(invoiceId, invoicesToNumberInfo, billingRun);
+                invoiceService.recalculateDates(invoiceId);
+                invoiceService.assignInvoiceNumberAndIncrementBAInvoiceDate(invoiceId, invoicesToNumberInfo);
 
             } catch (Exception e) {
                 log.error("Failed to increment invoice date for invoice {}", invoiceId, e);

opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/InvoiceService.java
@@ -2400,7 +2400,7 @@ public class InvoiceService extends PersistenceService<Invoice> {
      */
     @JpaAmpNewTx
     @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
-    public void assignInvoiceNumberAndIncrementBAInvoiceDate(Long invoiceId, InvoicesToNumberInfo invoicesToNumberInfo, BillingRun billingRun) throws BusinessException {
+    public void assignInvoiceNumberAndIncrementBAInvoiceDate(Long invoiceId, InvoicesToNumberInfo invoicesToNumberInfo) throws BusinessException {
 
         Invoice invoice = findById(invoiceId);
         assignInvoiceNumberFromReserve(invoice, invoicesToNumberInfo);
@@ -2410,16 +2410,40 @@ public class InvoiceService extends PersistenceService<Invoice> {
         billingAccount = incrementBAInvoiceDate(invoice.getBillingRun(), billingAccount);
         // /!\ DO NOT REMOVE THIS LINE, A LasyInitializationException is throw and the invoice is not generated.
         billingAccount = billingAccountService.refreshOrRetrieve(billingAccount);
-        //recalculateDate(invoice.getB, billingRun);
         invoice = update(invoice);
+    }
+
+    @JpaAmpNewTx
+    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+    public void recalculateDates(Long invoiceId) {
+        Invoice invoice = findById(invoiceId);
+        BillingAccount billingAccount = invoice.getBillingAccount();
+        recalculateDate(invoice, invoice.getBillingRun(), billingAccount);
+        update(invoice);
+    }
+
+    public void recalculateDate(Invoice invoice, BillingRun billingRun, BillingAccount billingAccount) {
+        BillingCycle billingCycle = billingAccount.getBillingCycle();
+        if (billingRun != null) {
+            billingCycle = billingRun.getBillingCycle();
+        }
+        if (billingRun != null) {
+            int delay = billingCycle.getInvoiceDateDelayEL() == null ?
+                    0 :
+                    InvoiceService.resolveImmediateInvoiceDateDelay(billingCycle.getInvoiceDateDelayEL(), invoice, billingAccount);
+            Date invoiceDate = DateUtils.addDaysToDate(new Date(), delay);
+            invoiceDate = DateUtils.setTimeToZero(invoiceDate);
+            invoice.setInvoiceDate(invoiceDate);
+            setInvoiceDueDate(invoice, billingCycle);
+            setInitialCollectionDate(invoice, billingCycle, billingRun);
+        }
     }
 
     /**
      * Increment BA invoice date.
-     * 
+     *
      * @param billingRun
      * @param billingAccount Billing account
-     * 
      * @throws BusinessException business exception
      */
     private BillingAccount incrementBAInvoiceDate(BillingRun billingRun, BillingAccount billingAccount) throws BusinessException {

peeter-piegaze-sonarsource/core

opencell-admin/ejbs/src/main/java/org/meveo/admin/async/InvoicingAsync.java
@@ -213,7 +213,8 @@ public class InvoicingAsync {
                 break;
             }
             try {
-                invoiceService.assignInvoiceNumber(invoiceId, invoicesToNumberInfo, billingRun);
+                invoiceService.recalculateDates(invoiceId);
+                invoiceService.assignInvoiceNumber(invoiceId, invoicesToNumberInfo);
 
             } catch (Exception e) {
                 if (result != null) {

opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/InvoiceService.java
@@ -2464,7 +2464,7 @@ public class InvoiceService extends PersistenceService<Invoice> {
      */
     @JpaAmpNewTx
     @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
-    public void assignInvoiceNumber(Long invoiceId, InvoicesToNumberInfo invoicesToNumberInfo, BillingRun billingRun) throws BusinessException {
+    public void assignInvoiceNumber(Long invoiceId, InvoicesToNumberInfo invoicesToNumberInfo) throws BusinessException {
         Invoice invoice = findById(invoiceId);
         assignInvoiceNumberFromReserve(invoice, invoicesToNumberInfo);
 
@@ -2473,8 +2473,33 @@ public class InvoiceService extends PersistenceService<Invoice> {
         billingAccount = incrementBAInvoiceDate(invoice.getBillingRun(), billingAccount);
         // /!\ DO NOT REMOVE THIS LINE, A LasyInitializationException is throw and the invoice is not generated.
         billingAccount = billingAccountService.refreshOrRetrieve(billingAccount);
-        //recalculateDate(invoice.getB, billingRun);
         invoice = update(invoice);
+    }
+
+    @JpaAmpNewTx
+    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+    public void recalculateDates(Long invoiceId) {
+        Invoice invoice = findById(invoiceId);
+        BillingAccount billingAccount = invoice.getBillingAccount();
+        recalculateDate(invoice, invoice.getBillingRun(), billingAccount);
+        update(invoice);
+    }
+
+    public void recalculateDate(Invoice invoice, BillingRun billingRun, BillingAccount billingAccount) {
+        BillingCycle billingCycle = billingAccount.getBillingCycle();
+        if (billingRun != null) {
+            billingCycle = billingRun.getBillingCycle();
+        }
+        if (billingRun != null) {
+            int delay = billingCycle.getInvoiceDateDelayEL() == null ?
+                    0 :
+                    InvoiceService.resolveImmediateInvoiceDateDelay(billingCycle.getInvoiceDateDelayEL(), invoice, billingAccount);
+            Date invoiceDate = DateUtils.addDaysToDate(new Date(), delay);
+            invoiceDate = DateUtils.setTimeToZero(invoiceDate);
+            invoice.setInvoiceDate(invoiceDate);
+            setInvoiceDueDate(invoice, billingCycle);
+            setInitialCollectionDate(invoice, billingCycle, billingRun);
+        }
     }
 
     /**
@@ -2482,7 +2507,6 @@ public class InvoiceService extends PersistenceService<Invoice> {
      *
      * @param billingRun
      * @param billingAccount Billing account
-     *
      * @throws BusinessException business exception
      */
     private BillingAccount incrementBAInvoiceDate(BillingRun billingRun, BillingAccount billingAccount) throws BusinessException {