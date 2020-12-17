opencellsoft/opencell-core

Review #5687 recalculate collection date invoice/AO

opencell-admin/ejbs/src/main/java/org/meveo/admin/async/InvoicingAsync.java
@@ -213,7 +213,7 @@ public class InvoicingAsync {
                 break;
             }
             try {
-                invoiceService.assignInvoiceNumberAndIncrementBAInvoiceDate(invoiceId, invoicesToNumberInfo);
+                invoiceService.assignInvoiceNumberAndIncrementBAInvoiceDate(invoiceId, invoicesToNumberInfo, billingRun);
 
             } catch (Exception e) {
                 log.error("Failed to increment invoice date for invoice {}", invoiceId, e);

opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/InvoiceService.java
@@ -2394,13 +2394,13 @@ public class InvoiceService extends PersistenceService<Invoice> {
     /**
      * Assign invoice number and increment BA invoice date.
      *
-     * @param invoiceId invoice id
+     * @param invoiceId            invoice id
      * @param invoicesToNumberInfo instance of InvoicesToNumberInfo
      * @throws BusinessException business exception
      */
     @JpaAmpNewTx
     @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
-    public void assignInvoiceNumberAndIncrementBAInvoiceDate(Long invoiceId, InvoicesToNumberInfo invoicesToNumberInfo) throws BusinessException {
+    public void assignInvoiceNumberAndIncrementBAInvoiceDate(Long invoiceId, InvoicesToNumberInfo invoicesToNumberInfo, BillingRun billingRun) throws BusinessException {
 
         Invoice invoice = findById(invoiceId);
         assignInvoiceNumberFromReserve(invoice, invoicesToNumberInfo);
@@ -2410,6 +2410,7 @@ public class InvoiceService extends PersistenceService<Invoice> {
         billingAccount = incrementBAInvoiceDate(invoice.getBillingRun(), billingAccount);
         // /!\ DO NOT REMOVE THIS LINE, A LasyInitializationException is throw and the invoice is not generated.
         billingAccount = billingAccountService.refreshOrRetrieve(billingAccount);
+        //recalculateDate(invoice.getB, billingRun);
         invoice = update(invoice);
     }
 

peeter-piegaze-sonarsource/core

opencell-admin/ejbs/src/main/java/org/meveo/admin/async/InvoicingAsync.java
@@ -213,7 +213,7 @@ public class InvoicingAsync {
                 break;
             }
             try {
-                invoiceService.assignInvoiceNumber(invoiceId, invoicesToNumberInfo);
+                invoiceService.assignInvoiceNumber(invoiceId, invoicesToNumberInfo, billingRun);
 
             } catch (Exception e) {
                 if (result != null) {
@@ -223,7 +223,7 @@ public class InvoicingAsync {
         }
         return new AsyncResult<String>("OK");
     }
-    
+
     /**
      * Increment BA invoice dates async. One BA at a time in a separate transaction.
      *
@@ -238,7 +238,7 @@ public class InvoicingAsync {
      */
     @Asynchronous
     @TransactionAttribute(TransactionAttributeType.NEVER)
-    public Future<String> incrementBAInvoiceDatesAsync(BillingRun billingRun, List<Long> baIds, 
+    public Future<String> incrementBAInvoiceDatesAsync(BillingRun billingRun, List<Long> baIds,
             Long jobInstanceId, JobExecutionResultImpl result, MeveoUser lastCurrentUser) {
 
         currentUserProvider.reestablishAuthentication(lastCurrentUser);
@@ -258,8 +258,8 @@ public class InvoicingAsync {
         }
         return new AsyncResult<String>("OK");
     }
-    
-    
+
+
     /**
      * Increment BA invoice dates async. One BA at a time in a separate transaction.
      *

opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/InvoiceService.java
@@ -2458,13 +2458,13 @@ public class InvoiceService extends PersistenceService<Invoice> {
     /**
      * Assign invoice number .
      *
-     * @param invoiceId invoice id
+     * @param invoiceId            invoice id
      * @param invoicesToNumberInfo instance of InvoicesToNumberInfo
      * @throws BusinessException business exception
      */
     @JpaAmpNewTx
     @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
-    public void assignInvoiceNumber(Long invoiceId, InvoicesToNumberInfo invoicesToNumberInfo) throws BusinessException {
+    public void assignInvoiceNumber(Long invoiceId, InvoicesToNumberInfo invoicesToNumberInfo, BillingRun billingRun) throws BusinessException {
         Invoice invoice = findById(invoiceId);
         assignInvoiceNumberFromReserve(invoice, invoicesToNumberInfo);
 
@@ -2473,6 +2473,7 @@ public class InvoiceService extends PersistenceService<Invoice> {
         billingAccount = incrementBAInvoiceDate(invoice.getBillingRun(), billingAccount);
         // /!\ DO NOT REMOVE THIS LINE, A LasyInitializationException is throw and the invoice is not generated.
         billingAccount = billingAccountService.refreshOrRetrieve(billingAccount);
+        //recalculateDate(invoice.getB, billingRun);
         invoice = update(invoice);
     }
 